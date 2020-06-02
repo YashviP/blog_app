1b06c7804500037a8c975e8026077a9b2a9fa6fc
yashvi <yashvipatel2903@gmail.com>
2020-06-02 20:42:03 +0530

    search users

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,15 +2,20 @@ class DashboardController < ApplicationController
 
 	def search
 
-      @names=params[:search].split(' ') if params[:search]
-     
-      if @names.length() ==1 
-        @search_users = User.where("first_name LIKE ?", "%#{@names[0]}")
-      elsif @names.length() >1
-        @search_users = User.where("first_name LIKE ? OR last_name LIKE ?", "%#{@names[0]}","%#{@names[1]}") 
-      end
-      
-	 end
+        @names=params[:search].split(' ')
+        if @names.length==1
+          @search_users = User.where("first_name  LIKE ?","%#{@names[0]}%") 
+          @search_users+=User.where("last_name  LIKE ?","%#{@names[0]}%") 
+         
+          elsif @names.length>1 
+            @search_users = User.where("first_name LIKE ? OR last_name LIKE ?", "%#{@names[0]}%","%#{@names[1]}%") 
+         
+
+
+        end
+    
+
+	end
 
 	def index
   	
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'dashboard/search',to: 'dashboard#search'
+  get '/search' => 'dashboard#search',as: 'search'
   root 'dashboard#index' , as: 'home'
   resources :post 
 end
